0080b868f7564dc05eb29ce491291cfab81671cb
Swalty13 <waltys@wit.edu>
2026-06-29 11:04:22 -0400

    feat: added model, schema, and first router for friends implementation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import users, sessions, analytics, routes, route_attempts
+from app.routers import users, sessions, analytics, routes, route_attempts, friends
 from app.database import Base, engine
 
 import app.models.users
 import app.models.sessions
 import app.models.routes
 import app.models.attempts
+import app.models.friends
 
 app = FastAPI()
 
@@ -23,6 +24,7 @@ app.include_router(users.router)
 app.include_router(analytics.router)
 app.include_router(routes.router)
 app.include_router(route_attempts.router)
+app.include_router(friends.router)
 
 
 @app.get("/")
---
+++ backend/app/routers/friends.py
@@ -0,0 +1,47 @@
+from uuid import UUID
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session as DBSession
+
+from app.database import get_db
+from app.auth import get_current_user
+from app.models.users import User
+from app.models.friends import FriendRequest
+from app.schemas.schema import FriendRequestCreate, FriendRequestReponse
+
+router = APIRouter(prefix = "/friends", tags=["friends"])
+
+@router.post("/request")
+def send_friend_request(friend_data:FriendRequestCreate, db: DBSession = Depends(get_db), current_user=Depends(get_current_user)):
+    reciever = db.query(User).filter(User.username == friend_data.username).first()
+
+    if not reciever:
+        raise HTTPException(status_code=400, detail="User not found")
+    
+    if reciever.id == current_user["user_id"]:
+        raise HTTPException(status_code=400, detail="You cannot add yourself as a friend")
+    
+    existing_request = db.query(FriendRequest).filter(FriendRequest.sender_id.in_([current_user["user_id"], reciever.id]), FriendRequest.receiver_id.in_([current_user["user_id"], reciever.id])).first()
+
+    if existing_request:
+        raise HTTPException(status_code=400, detail="There is already a pending friend request")
+    
+    friend_request = FriendRequest(sender_id=current_user["user_id"], receiver_id = reciever.id, status="pending")
+
+    db.add(friend_request)
+    db.commit()
+    db.refresh(friend_request)
+
+    return friend_request
+
+@router.get("/requests")
+def get_friend_requests(db: DBSession = Depends(get_db), current_user=Depends(get_current_user)):
+
+
+@router.post("/accept/{request_id}")
+def accept_friend_request(request_id: UUID, db: DBSession = Depends(get_db), current_user=Depends(get_current_user)):
+
+
+@router.get("/")
+def get_friends(db: DBSession = Depends(get_db), current_user=Depends(get_current_user)):
+    
